e27ba9c40fe2a15f5718757a161b9d652102c905
ajaykrishnan33 <akj1996@gmail.com>
2018-04-13 23:44:34 +0530

    checkpoint2

--- blogger/models.py
+++ blogger/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+	title = models.CharField(max_length=500)
+	content = models.TextField()
+	pub_date = models.DateTimeField('date published')
+
+	def __str__(self):
+		return self.title
+
+class Comment(models.Model):
+	blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+	text = models.CharField(max_length=1000)
+	pub_date = models.DateTimeField('date published')
+
+	def __str__(self):
+		return self.text
+
--- workshop/settings.py
+++ workshop/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'blogger'
+    'blogger.apps.BloggerConfig'
 ]
 
 MIDDLEWARE = [
