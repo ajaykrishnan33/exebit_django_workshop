aea0ef9a5b54be979334ccc0b59ec57423c497e9
ajaykrishnan33 <akj1996@gmail.com>
2018-04-14 01:14:49 +0530

    checkpoint3

--- blogger/views.py
+++ blogger/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, Http404
+from .models import Blog
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello World! Welcome to Blogger !")
+	blog_list = Blog.objects.order_by('-pub_date')
+	context = {"blog_list":blog_list}
+	return render(request, 'blogger/index.html', context)
+
+def detail(request, blog_id):
+	try:
+		blog = Blog.objects.get(pk=blog_id)
+		context = {"blog":blog}
+	except:
+		raise Http404("Blog does not exist")
+
+	return render(request, "blogger/detail.html", context)
+	# return render(request, template_name)
